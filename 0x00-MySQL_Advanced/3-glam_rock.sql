a57d00b5712e3265c309e8cb566dbd3e917f8fb8
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-05 01:04:11 +0300

    Corrected Syntax

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL Script that lists all bands with <Glam rock> as their main style
 -- ranked bt their longevity
-SELECT `band_name`, (IFNULL(`split`, '2020') - `formed`) AS `lifespan`
+SELECT `band_name`, (IFNULL(`split`, '2022') - `formed`) AS `lifespan`
 FROM `metal_bands`
 WHERE `style` LIKE '%Glam rock%'
 ORDER BY `lifespan` DESC;
